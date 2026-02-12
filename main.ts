9a5889434508234f248924f82add79f5d5ad1a75
Jose M. Caballero <96978309+josemcaba@users.noreply.github.com>
2026-02-13 00:28:32 +0100

    .

--- main.ts
+++ main.ts
@@ -1,15 +1,3 @@
-function DatosSmartIoT () {
-    ESP8266_IoT.setSmartIotUploadData(
-    Environment.ReadLightIntensity(AnalogPin.P0),
-    Environment.octopus_BME280(Environment.BME280_state.BME280_temperature_C),
-    Environment.octopus_BME280(Environment.BME280_state.BME280_humidity),
-    Environment.octopus_BME280(Environment.BME280_state.BME280_pressure)
-    )
-    ESP8266_IoT.uploadSmartIotData()
-}
-ESP8266_IoT.iotSwitchEvent(ESP8266_IoT.SmartIotSwitchState.off, function () {
-    basic.showIcon(IconNames.No)
-})
 function DatosOLED () {
     OLED.clear()
     OLED.newLine()
@@ -25,24 +13,32 @@ function DatosOLED () {
     OLED.writeString("Presion    : ")
     OLED.writeNumNewLine(Environment.octopus_BME280(Environment.BME280_state.BME280_pressure))
 }
-ESP8266_IoT.iotSwitchEvent(ESP8266_IoT.SmartIotSwitchState.on, function () {
-    basic.showIcon(IconNames.Yes)
-})
+function DatosThingSpeak () {
+    ESP8266_IoT.setData(
+    "your_write_api_key",
+    Environment.ReadLightIntensity(AnalogPin.P0),
+    Environment.octopus_BME280(Environment.BME280_state.BME280_temperature_C),
+    Environment.octopus_BME280(Environment.BME280_state.BME280_humidity),
+    Environment.octopus_BME280(Environment.BME280_state.BME280_pressure)
+    )
+    ESP8266_IoT.uploadData()
+}
+OLED.init(128, 64)
+OLED.clear()
 basic.showIcon(IconNames.Sad)
 ESP8266_IoT.initWIFI(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
 while (ESP8266_IoT.wifiState(false)) {
     ESP8266_IoT.connectWifi("DIGIFIBRA-AKHH", "SKtkPPXYRHKs")
 }
-OLED.init(128, 64)
 basic.forever(function () {
-    while (ESP8266_IoT.smartiotState(false)) {
+    DatosOLED()
+    while (ESP8266_IoT.thingSpeakState(false)) {
         DatosOLED()
         basic.showIcon(IconNames.Heart)
-        ESP8266_IoT.connectSmartiot("qokJCxP2vy9pWuTAGYvc", "1")
+        ESP8266_IoT.connectThingSpeak()
         basic.showIcon(IconNames.SmallHeart)
     }
-    DatosOLED()
-    DatosSmartIoT()
+    DatosThingSpeak()
     basic.showIcon(IconNames.Happy)
     basic.pause(30000)
 })
